8abb14e37598b9ef5ffd03724e482bf53ed7e7d5
Arthur521 <arthurassis@hotmail.com.br>
2025-02-23 18:30:34 -0300

    data-hora

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 // objeto do usuário
-const usuario = { nome: "Raphael", matricula: "123456", pendencia: false, acessibilidade: true, dataHora: "0" };
+const usuario = { nome: "Raphael", matricula: "123456", pendencia: false, acessibilidade: true, dataHora: 0 };
 
 // lista objetos de armários
 const armarios = [
-  { id: 1, formato: "padrao", status: true, acessivel: false, dataHora: "0" },
-  { id: 2, formato: "padrao", status: true, acessivel: false, dataHora: "0" },
-  { id: 3, formato: "padrao", status: true, acessivel: false, dataHora: "0" },
-  { id: 4, formato: "padrao", status: false, acessivel: true, dataHora: "0" },
-  { id: 5, formato: "padrao", status: false, acessivel: true, dataHora: "0" },
-  { id: 6, formato: "duplo", status: true, acessivel: true, dataHora: "0" },
-  { id: 7, formato: "duplo", status: false, acessivel: true, dataHora: "0" },
-  { id: 8, formato: "duplo", status: false, acessivel: true, dataHora: "0" },  
+  { id: 1, formato: "padrao", status: true, acessivel: false, dataHora: 0 },
+  { id: 2, formato: "padrao", status: true, acessivel: false, dataHora: 0 },
+  { id: 3, formato: "padrao", status: true, acessivel: false, dataHora: 0 },
+  { id: 4, formato: "padrao", status: false, acessivel: true, dataHora: 0 },
+  { id: 5, formato: "padrao", status: false, acessivel: true, dataHora: 0 },
+  { id: 6, formato: "duplo", status: true, acessivel: true, dataHora: 0 },
+  { id: 7, formato: "duplo", status: false, acessivel: true, dataHora: 0 },
+  { id: 8, formato: "duplo", status: false, acessivel: true, dataHora: 0 },  
 ];
 
 // função para reserva do armário, incluindo as regras.
@@ -31,7 +31,7 @@ function reservarArmario() {
   // Caso exista armário(s) disponíveil, seguimos sorteando uma opção. 
   let armarioSorteado = armariosDisponiveis[Math.floor(Math.random() * armariosDisponiveis.length)];
 
-  armarioSorteado.dataHora = new Date();
+  armarioSorteado.dataHora = Date.now();
   
   // Depois localizamos o armário emprestado na lista de armarios e mudamos o status do armário.
   let armarioEmprestado = armarios.find(armario => armario.id === armarioSorteado.id).status = false;
