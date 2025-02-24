6ab930d1a8e22a81ef844a8947a1c65e55bab7ce
Arthur521 <arthurassis@hotmail.com.br>
2025-02-24 16:02:56 -0300

    quarto commit

--- scripts.js
+++ scripts.js
@@ -1,48 +1,38 @@
 // objeto do usuário
-const usuario = { nome: "Raphael", matricula: "123456", pendencia: false, acessibilidade: true, dataHora: 0 };
+const usuario = { nome: "Raphael", matricula: "123456", pendencia: false, acessibilidade: true, dataHora: 0, dataHoraEntrega: 0 };
 
 // lista objetos de armários
 const armarios = [
-  { id: 1, formato: "padrao", status: true, acessivel: false, dataHora: 0 },
-  { id: 2, formato: "padrao", status: true, acessivel: false, dataHora: 0 },
-  { id: 3, formato: "padrao", status: true, acessivel: false, dataHora: 0 },
-  { id: 4, formato: "padrao", status: false, acessivel: true, dataHora: 0 },
-  { id: 5, formato: "padrao", status: false, acessivel: true, dataHora: 0 },
-  { id: 6, formato: "duplo", status: true, acessivel: true, dataHora: 0 },
-  { id: 7, formato: "duplo", status: false, acessivel: true, dataHora: 0 },
-  { id: 8, formato: "duplo", status: false, acessivel: true, dataHora: 0 },  
+  { id: 1, formato: "padrao", status: true, acessivel: false, dataHora: 0, dataHoraEntrega: 0 },
+  { id: 2, formato: "padrao", status: true, acessivel: false, dataHora: 0, dataHoraEntrega: 0 },
+  { id: 3, formato: "padrao", status: true, acessivel: false, dataHora: 0, dataHoraEntrega: 0 },
+  { id: 4, formato: "padrao", status: false, acessivel: true, dataHora: 0, dataHoraEntrega: 0 },
+  { id: 5, formato: "padrao", status: false, acessivel: true, dataHora: 0, dataHoraEntrega: 0 },
+  { id: 6, formato: "duplo", status: true, acessivel: true, dataHora: 0, dataHoraEntrega: 0 },
+  { id: 7, formato: "duplo", status: false, acessivel: true, dataHora: 0, dataHoraEntrega: 0 },
+  { id: 8, formato: "duplo", status: false, acessivel: true, dataHora: 0, dataHoraEntrega: 0 },  
 ];
 
 // função para reserva do armário, incluindo as regras.
 function reservarArmario() {
-  
-  // obter tipo de armário selecionado pelo usuário no html.
   let tipoSelecionado = document.getElementById("tipoArmario").value;
   
-  // na lista, filtrar apenas os armários que estão disponíveis e que são acessiveis ao usuário.
   let armariosDisponiveis = armarios.filter(a => a.formato === tipoSelecionado && a.status === true && usuario.acessibilidade === a.acessivel);
   
-  // caso não exista armário disponível, retorna para o usuário mensagem.
   if (armariosDisponiveis.length === 0) {
     document.getElementById("resultado").innerText = `Olá, ${usuario.nome}! Nenhum armário disponível para o tipo selecionado.`;
     return;
   }
   
-  // Caso exista armário(s) disponíveil, seguimos sorteando uma opção. 
   let armarioSorteado = armariosDisponiveis[Math.floor(Math.random() * armariosDisponiveis.length)];
-
+  
   armarioSorteado.dataHora = Date.now();
-  
-  // Depois localizamos o armário emprestado na lista de armarios e mudamos o status do armário.
-  let armarioEmprestado = armarios.find(armario => armario.id === armarioSorteado.id).status = false;
-  
-  // Finalmente, mudamos a pendencia do usuário para verdadeira.
+  armarioSorteado.dataHoraEntrega = dataHora + 86400000;
+  armarios.find(armario => armario.id === armarioSorteado.id).status = false;
   usuario.pendencia = true;
   
-  // Impmimimos uma mensagem de reserva para o usuário.
-  document.getElementById("resultado").innerText = `Olá, ${usuario.nome}! O armário ${armarioSorteado.id} foi reservado com sucesso, agora as ${armarioSorteado.dataHora}!`;
+  document.getElementById("resultado").innerText = `Olá, ${usuario.nome}! O armário ${armarioSorteado.id} foi reservado com sucesso, agora às ${new Date(armarioSorteado.dataHora).toLocaleString("pt-BR")}. \nA data e hora de entrega máxima será ${armarioSorteado.dataHoraEntrega}`;
 
   console.log(usuario);
   console.log(armarios);
-
 }
